0a0516415619be691d780150c01a8795cfb76753
Samuel Park <s.park111497@gmail.com>
2022-10-20 21:25:55 -0700

    finished assignment

--- server/app.js
+++ server/app.js
@@ -4,5 +4,8 @@ const app = express();
 const peopleRouter = require('./routes/people');
 app.use('/people', peopleRouter);
 
+const colors = require('./routes/colors');
+app.use('/colors', colors);
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
